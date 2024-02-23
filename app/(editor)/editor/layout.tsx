3650b1fa4ac04db71030fe314162aa86d96f6255
Giacomo Materozzi <giacomo.materozzi@docebo.com>
2024-02-24 00:40:35 +0100

    feat(translation): added project settings

--- app/(editor)/editor/layout.tsx
+++ app/(editor)/editor/layout.tsx
@@ -4,8 +4,6 @@ interface EditorProps {
 
 export default function EditorLayout({ children }: EditorProps) {
   return (
-    <div className="container mx-auto grid items-start gap-10 py-8">
-      {children}
-    </div>
+    <div className="container mx-auto items-start gap-10 py-8">{children}</div>
   )
 }
