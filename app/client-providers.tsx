b8c21dcba01aa409f82bc2ab4e009fa3de6491f6
Giacomo Materozzi <giacomo.materozzi@docebo.com>
2024-03-11 10:28:53 +0100

    feat(general): added head loading

--- app/client-providers.tsx
+++ app/client-providers.tsx
@@ -1,10 +1,21 @@
 "use client"
 
 import { SessionProvider } from "next-auth/react"
+import { AppProgressBar as ProgressBar } from "next-nprogress-bar"
 
 import i18n from "@/lib/i18n"
 
 export default function ClientProvider(props: { children: React.ReactNode }) {
   i18n.changeLanguage("en")
-  return <SessionProvider>{props.children}</SessionProvider>
+  return (
+    <SessionProvider>
+      {props.children}
+      <ProgressBar
+        height="2px"
+        color="#44403c"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </SessionProvider>
+  )
 }
